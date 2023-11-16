fe15fa0a6902b5e5303a783de27272ecba63f242
Justin <justinl@starpilwax.com>
2023-11-15 22:16:35 -0500

    finished header

---
+++ Documents/websites/trello-clone/app/page.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/Header'
+
+
+export default function Home() {
+  return (
+    <main>
+       {/* Header */}
+      <Header />
+
+
+      {/* Board */}
+      <h1>Trello Clone</h1>
+    </main>
+  )
+}
